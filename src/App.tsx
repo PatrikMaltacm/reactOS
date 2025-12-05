a11bee109c624231e684d34833e29101984ec826
Patrick Carvalho Malta <patrick.malta@dcoimbra.com.br>
2025-12-05 10:23:15 -0400

    feat: remove component unused

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import BlackJack from './components/BlackJack'
 import styles from "./App.module.css"
 import Files from './components/Files'
 import TextEditor from './components/TextEditor'
-import { useContext } from 'react'
-
-import { OpenedWindowsContext } from './context/OpenedWindowsContext'
-import ChangeOpenedWindows from './components/ChangeOpenedWindows'
 import TaskBar from './components/TaskBar'
 
 function App() {
 
   return (
     <div className={styles.desktop}>
-      <ChangeOpenedWindows />
       <TextEditor />
       <Files />
       <BlackJack />
